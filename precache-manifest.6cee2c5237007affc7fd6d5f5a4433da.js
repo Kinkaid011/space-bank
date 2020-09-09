ca7f6319963ad86f1373deff0c986ed4d3157a8e
Kinkaid011 <k.nolen46@gmail.com>
2020-09-09 12:12:10 -0400

    Updates

---
+++ precache-manifest.6cee2c5237007affc7fd6d5f5a4433da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39cc3376d0bcbc51cf33ecad212bcfd7",
+    "url": "/space-bank/index.html"
+  },
+  {
+    "revision": "742e4d142d99869d8ce4",
+    "url": "/space-bank/static/css/main.b06cc6b7.chunk.css"
+  },
+  {
+    "revision": "e5d49c1d3019e9f2ccaa",
+    "url": "/space-bank/static/js/2.34ba475c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/space-bank/static/js/2.34ba475c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "742e4d142d99869d8ce4",
+    "url": "/space-bank/static/js/main.1676e14c.chunk.js"
+  },
+  {
+    "revision": "dec8eaa91e8503cce4e6",
+    "url": "/space-bank/static/js/runtime-main.93f94ebd.js"
+  }
+]);
